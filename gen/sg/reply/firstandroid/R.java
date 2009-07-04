6a3a4629a00203df6de0dec26143b3bb1618ebd8
Jason Ong <jasonong@jasonong-netbook.(none)>
2009-07-04 16:39:26 +0800

    Added test layout

--- gen/sg/reply/firstandroid/R.java
+++ gen/sg/reply/firstandroid/R.java
@@ -10,6 +10,11 @@ package sg.reply.firstandroid;
 public final class R {
     public static final class attr {
     }
+    public static final class id {
+        public static final int entry=0x7f040001;
+        public static final int label=0x7f040000;
+        public static final int ok=0x7f040002;
+    }
     public static final class layout {
         public static final int main=0x7f020000;
     }
